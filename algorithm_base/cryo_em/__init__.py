d1cf18b54859746dece3f04314cb5d00510fc246
Abraham Chengshuai Yang <integrityyang@gmail.com>
2026-03-20 08:44:09 -0700

    fix: correct cryo_em __init__.py import (run_relion_dl → run_relion)

--- algorithm_base/cryo_em/__init__.py
+++ algorithm_base/cryo_em/__init__.py
@@ -2,6 +2,6 @@
 from .solvers import run_solver, list_solvers, SOLVERS, MODALITY_ID, DISPLAY_NAME
 from .solvers import run_traditional_cpu
 from .solvers import run_best_quality
-from .solvers import run_relion_dl
+from .solvers import run_relion
 
 __all__ = ["run_solver", "list_solvers", "SOLVERS", "MODALITY_ID", "DISPLAY_NAME"]
